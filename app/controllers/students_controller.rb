c091739063119135726f4cb4aa5896e42c3e0793
Tara Sabella <tarasabella@gmail.com>
2019-01-14 03:24:04 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,16 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
+  
   def index
     @students = Student.all
-    render :index
   end
-  
-  def show 
-    @students = Student.find_by_id(params)
-    erb :show 
-  end 
+
+  def show
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: :index
   
-  def index 
-    @students = Student.all
-    render :index 
-  end
 
-def show 
-  @students = Student.find_by_id(params)
-  render :show 
+     get "students/:id", to: "students#show"
 end 
